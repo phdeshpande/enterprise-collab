b358742fde29892e9679263b3c5d0fedf61cb098
Purushottam Deshpande <purushottam@bitlasoft.com>
2013-12-22 22:55:46 +0530

    Rails 4.0.2 base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnterpriseCollab::Application.config.secret_key_base = '885ddef967e10aed31e5a98455bd2af3df62d44d5e080a87175a214bb2ec0a426d63a6b9d5bbbc7e2a0b63e2d9a4cc84ca47a682ea20051fe0cae5b532ef4e9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnterpriseCollab::Application.config.session_store :cookie_store, key: '_enterprise-collab_session'
