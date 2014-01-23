750742c0fdd9e2917478f95436c0babfae3070ed
Purushottam D <purushottam@hutgeeks.com>
2014-01-23 12:24:06 +0530

    Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 EnterpriseCollab::Application.routes.draw do
   get "/register" => 'home#register'
+  resources :dashboard
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
