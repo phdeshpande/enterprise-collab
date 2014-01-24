ac2f9073255c80a809a9fe33f6086008057d7428
Purushotham D <purushotham@hutgeeks.com>
2014-01-25 00:08:41 +0530

    Login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 EnterpriseCollab::Application.routes.draw do
-  get "/register" => 'home#register'
+  #get "/register" => 'home#register'
   resources :dashboard
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
