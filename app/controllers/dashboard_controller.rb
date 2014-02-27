248922394a9e86c39ba2c8e2b042e452740f0bb3
Purushottam D <purushottam@hutgeeks.com>
2014-02-27 17:06:24 +0530

    h_listing_users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+  	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 EnterpriseCollab::Application.routes.draw do
   
+  get "administration/manage_users"
+  get "administration/manage_accounts"
   # devise_scope :user do
     # match "users/sign_in" => "home#index", :via => [:get, :post]
   # end  
   devise_for :users, :controllers => {:registrations => "users/registrations"}
   resources :dashboard
   resources :spaces
+  resources :administration
 
   # get "/register" => 'home#register'
   # The priority is based upon order of creation: first created -> highest priority.
