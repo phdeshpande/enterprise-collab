6ec902d99937c4ec9592d05d821e4231e5df3219
Purushottam D <purushottam@hutgeeks.com>
2014-03-01 13:09:29 +0530

    added modal dialog to crete user with form in it

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,7 +1,7 @@
 class AdministrationController < ApplicationController
   def index
-   @users=User.all
-  end 	
+  	@users=User.all;
+   end 	
 
   def manage_users
   end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
   has_many :users, :dependent => :destroy
 
-  validates :company_name, :presence => true, uniqueness :true
+  validates :company_name, :presence => true
 end
